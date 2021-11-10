4917b6ea5fe8c958364156b4611314f03dfba663
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-10 15:30:06 +0100

    esercizio con bonus

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello World! I\'m using Vue.js!',
+    imageUrl: 'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png' 
+  }
+});
